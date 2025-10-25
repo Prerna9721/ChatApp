e931d78f609b49c39d69c533c95500a2c0a300e1
Prerna Sinha <prernasinha9511@gmail.com>
2025-10-25 13:29:28 +0530

    Commit

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-export const baseURL="http://chatbackend-production-f10d.up.railway.app/"
- 
+export const baseURL="chatbackend-production-f10d.up.railway.app";
+
 export const httpClient =axios.create({
     baseURL:baseURL,
 });
